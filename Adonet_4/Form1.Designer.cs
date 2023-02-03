40d403e57cd6975bdf4920779b5432d2388a90fb
Goktug YILDIZ <102765970+GktgYildiz@users.noreply.github.com>
2023-02-03 20:27:17 +0300

    Add files via upload

---
+++ Adonet_4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Adonet_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(785, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MARS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 163);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 137);
+            this.textBox2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(264, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(533, 340);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KATEGORİ ADI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AÇIKLAMA";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "INSERT(connected)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "INSERT(disconnected)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Adonet_4/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Adonet_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection conn = new SqlConnection("Server=DESKTOP-MUMOMKH;database=Northwind;Trusted_Connection=True;MultipleActiveResultSets = True;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu1 = "select CategoryName, Description from Categories";
+            string sorgu2 = "select ProductName,UnitPrice,UnitsInStock from Products";
+
+            SqlCommand cmd1 = new SqlCommand(sorgu1, conn);
+            SqlCommand cmd2 = new SqlCommand(sorgu2, conn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                SqlDataReader dr = cmd1.ExecuteReader();
+                SqlDataReader dr2 = cmd2.ExecuteReader();
+                MessageBox.Show("işlem başarılı");
+            }
+            else
+            {
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu1 = "select CategoryName, Description from Categories";
+            string sorgu2 = "Insert Categories(CategoryName,Description) Values(@catName,@desc)";
+            SqlCommand cmd1 = new SqlCommand(sorgu1, conn);
+            SqlCommand cmd2 = new SqlCommand(sorgu2, conn);
+
+            cmd2.Parameters.AddWithValue("@catName", textBox1.Text);
+            cmd2.Parameters.AddWithValue("@desc", textBox2.Text);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                cmd2.ExecuteNonQuery();
+                SqlDataReader dr = cmd1.ExecuteReader();
+                DataTable dt = new DataTable();
+                if (dr.HasRows)
+                {
+                    //while (dr.Read())
+                    //{
+
+                    //}
+                    dt.Load(dr);
+                    dataGridView1.DataSource = dt;
+                    MessageBox.Show("işlem başarılı");
+                }
+            }
+            else
+            {
+                conn.Close();
+            }
+        }
+        SqlDataAdapter dap;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string sorgu1 = "select CategoryName, Description from Categories";
+            string sorgu2 = "Insert Categories(CategoryName,Description) Values(@catName,@desc)";
+            dap = new SqlDataAdapter(sorgu1, conn);
+            SqlCommand cmd = new SqlCommand(sorgu2, conn);
+
+            dap.InsertCommand = cmd;
+            dap.InsertCommand.Parameters.AddWithValue("@catName", textBox1.Text);
+            dap.InsertCommand.Parameters.AddWithValue("@desc", textBox2.Text);
+            conn.Open();
+
+            dap.InsertCommand.ExecuteNonQuery();
+
+            conn.Close();
+
+
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+                MessageBox.Show("işlem başarılı");
+
+        }
+    }
+}
